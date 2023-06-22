ecee2f8b869cbb20e4cbb19b2b96b9736e5305b9
Den Seksan <den@finnomena.com>
2023-06-22 18:23:57 +0700

    first commit

---
+++ display_image_from_url/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  display_image_from_url
+//
+//  Created by Den Seksan on 22/6/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ display_image_from_url/display_image_from_urlApp.swift
@@ -0,0 +1,17 @@
+//
+//  display_image_from_urlApp.swift
+//  display_image_from_url
+//
+//  Created by Den Seksan on 22/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct display_image_from_urlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
