fa6ed7e723c5abd79405da959fa04ef569e86fcc
Den Seksan <den@finnomena.com>
2023-06-22 18:50:07 +0700

    Add ImageView

---
+++ display_image_from_url/Views/ImageView.swift
@@ -0,0 +1,20 @@
+//
+//  ImageView.swift
+//  display_image_from_url
+//
+//  Created by Den Seksan on 22/6/2566 BE.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView()
+    }
+}
